812073da77fe7874235820a7dd00ce062b462abe
Ben Matheja <post@benmatheja.de>
2020-07-12 10:28:12 +0200

    apply flake8 conventions

--- handlers/todoist_handler.py
+++ handlers/todoist_handler.py
@@ -1,7 +1,10 @@
 import json
 import logging
 import os
-import base64, hmac, hashlib, http.client
+import base64
+import hmac
+import hashlib
+import http.client
 import datetime
 from pytz import timezone
 from todoist.api import TodoistAPI
@@ -12,10 +15,13 @@ WORKING_MINUTES = 43
 REQUIRED_WORKING_HOURS = 8
 INTERMITTENT_FASTING_HOURS = 16
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
 def get_token():
+
     if os.getenv('TODOIST_APIKEY'):
         return os.getenv('TODOIST_APIKEY')
     else:
@@ -23,22 +29,27 @@ def get_token():
 
 
 def get_clientsecret():
+
     if os.getenv('TODOIST_CLIENTSECRET'):
         return os.getenv('TODOIST_CLIENTSECRET')
     else:
         return '3b3e61devsecret'
 
+
 """
-Routine to compute the X-Todoist-Hmac-SHA256	
-To verify each webhook request was indeed sent by Todoist, 
-an X-Todoist-Hmac-SHA256 header is included; 
-it is a SHA256 Hmac generated using your client_secret as the encryption key 
-and the whole request payload as the message to be encrypted. 
+Routine to compute the X-Todoist-Hmac-SHA256
+To verify each webhook request was indeed sent by Todoist,
+an X-Todoist-Hmac-SHA256 header is included;
+it is a SHA256 Hmac generated using your client_secret as the encryption key
+and the whole request payload as the message to be encrypted.
 The resulting Hmac would be encoded in a base64 string.
 """
-def compute_hmac(body, todoist_clientsecret = get_clientsecret()):
-    #logger.info("compute_hmac with %s ", body)
-    signature =  base64.b64encode(hmac.new(
+
+
+def compute_hmac(body, todoist_clientsecret=get_clientsecret()):
+
+    # logger.info("compute_hmac with %s ", body)
+    signature = base64.b64encode(hmac.new(
         todoist_clientsecret.encode('utf-8'),
         body.encode('utf-8'),
         digestmod=hashlib.sha256).digest()).decode('utf-8')
@@ -46,10 +57,13 @@ def compute_hmac(body, todoist_clientsecret = get_clientsecret()):
 
 
 def extract_useragent(event):
+
     return event.get('headers')['User-Agent']
 
+
 def extract_delivered_hmac(event):
-   return event.get('headers')['x-todoist-hmac-sha256']
+
+    return event.get('headers')['x-todoist-hmac-sha256']
 
 
 """
@@ -59,12 +73,14 @@ Handle the incoming Event
 * Compute HMAC
 * Work on Event
 """
+
+
 def handle_event(event, context):
     logger.info('ENVIRONMENT VARIABLES: %s', os.environ)
     logger.info('EVENT: %s', event)
 
-    if  event.get('body') and extract_useragent(event) == 'Todoist-Webhooks':
-        #logger.info('Request has body and useragent is correct')
+    if event.get('body') and extract_useragent(event) == 'Todoist-Webhooks':
+        # logger.info('Request has body and useragent is correct')
 
         delivered_hmac = extract_delivered_hmac(event)
         computed_hmac = compute_hmac(event.get('body'))
@@ -77,27 +93,30 @@ def handle_event(event, context):
             if json_body.get('event_data')['content'] == "Kommen Zeit notieren":
                 logger.info("Received a Clock-In Event")
                 create_todoist_clockout_task()
-                response =  {"status": http.client.responses[http.client.CREATED]}
+                response = {"status": http.client.responses[http.client.CREATED]}
             if json_body.get('event_data')['content'] == "Last Meal finished":
                 logger.info("Received a Last Meal Event")
                 create_todoist_lastmeal_task()
-                response =  {"status": http.client.responses[http.client.CREATED]}
+                response = {"status": http.client.responses[http.client.CREATED]}
             else:
-                response =  {"status": http.client.responses[http.client.NO_CONTENT]}
+                response = {"status": http.client.responses[http.client.NO_CONTENT]}
         else:
-           response = {"status": http.client.responses[http.client.UNAUTHORIZED]} 
+            response = {"status": http.client.responses[http.client.UNAUTHORIZED]}
     else:
         response = {"status": http.client.responses[http.client.BAD_REQUEST]}
-    
+
     return response
 
 
 """
 Routine to create a new Todoist Clock-Out Task using the Sync API
 """
+
+
 def create_todoist_clockout_task():
     now = datetime.datetime.now().astimezone(timezone('Europe/Amsterdam'))
-    soll = datetime.timedelta(hours=REQUIRED_WORKING_HOURS, minutes=WORKING_MINUTES) + now
+    soll = datetime.timedelta(hours=REQUIRED_WORKING_HOURS,
+                              minutes=WORKING_MINUTES) + now
     acc = datetime.timedelta(hours=WORKING_HOURS, minutes=WORKING_MINUTES) + now
     clockin_time = str(now.hour) + ':' + str('%02d' % now.minute)
     clockout_time = str(acc.hour) + ':' + str('%02d' % acc.minute)
@@ -119,12 +138,15 @@ def create_todoist_clockout_task():
     if api.commit():
         logger.info("Todoist Clock-Out Task has been created")
 
+
 """
 Routine to create a new Todoist Last Meal Task using the Sync API
 * Take current time
 * add 16 hours to it
 * Create a new Task 16h in the future
 """
+
+
 def create_todoist_lastmeal_task():
     now = datetime.datetime.now().astimezone(timezone('Europe/Amsterdam'))
     cleared_time = datetime.timedelta(hours=INTERMITTENT_FASTING_HOURS) + now
@@ -135,12 +157,16 @@ def create_todoist_lastmeal_task():
     task_content = ' Intermittent Fasting - Cleared to eat (Check in: ' + checkin_time + ')'
 
     logger.info('Create Todoist LastMeal Task: ' + task_content + ' due at: ' + checkout_time)
-    
+
     api = TodoistAPI(token=get_token(), cache="/tmp/todoist")
     logger.info("Trying to connect to Todoist API with: %s", get_token())
     if not api.sync():
         logger.warning('Todoist: API Sync failed')
         exit()
-    api.items.add(task_content, project_id='1509802153', date_string=checkout_time, labels=[2154004914], priority=3)
+    api.items.add(task_content,
+                  project_id='1509802153',
+                  date_string=checkout_time,
+                  labels=[2154004914],
+                  priority=3)
     if api.commit():
         logger.info("Todoist LastMeal Task has been created")
